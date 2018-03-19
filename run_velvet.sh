aae52adf9bb2fa573ab98e4582658278aae1e269
dougc333 <dougc@stanfordalumni.org>
2018-03-18 19:39:50 -0700

    add

---
+++ run_velvet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#make 'CATEGORIES=57' 'MAXKMERLENGTH=201' 'BIGASSEMBLY=1' 'LONGSEQUENCES=1' 'OPENMP=1' 
+./velveth dir 31 -shortPaired -separate -fastq /home/dc/Downloads/r1.fastq.gz /home/dc/Downloads/r2.fastq.gz
+
